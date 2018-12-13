598861757f8821b54e2bef84367ce18c93248f23
joelbarenco@gmail.com <ebrosetup@gmail.com>
2018-12-13 19:45:37 +0000

    added contactus page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishDetailComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
@@ -41,7 +42,6 @@ class Main extends Component {
           />
       );
     }
-
     return (
       <div>
         <Header />
@@ -49,6 +49,7 @@ class Main extends Component {
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
+          <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
